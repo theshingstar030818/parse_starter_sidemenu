9eb826ae98dce1b85e03aca0e8a4b7e02cbaf57d
tanzeel <tanzeelrana@cmail.carleton.ca>
2016-11-05 19:35:47 -0400

    change defaul route page to login page

--- www/js/app.js
+++ www/js/app.js
@@ -112,5 +112,5 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     ;
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/components');
+    $urlRouterProvider.otherwise('/login');
 });
